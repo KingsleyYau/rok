c6bbee0e3e5371e063965a716b700eccdd78c91d
Kingsley Yau <Kingsleyyau@gmail.com>
2024-05-28 15:46:12 +0800

    Sync

--- tasks/GetPlayerName.py
+++ tasks/GetPlayerName.py
@@ -3,6 +3,7 @@ from tasks.constants import TaskName
 from filepath.file_relative_paths import ImagePathAndProps
 
 import re
+import time
 
 class GetPlayerName(Task):
     def __init__(self, bot):
@@ -14,11 +15,16 @@ class GetPlayerName(Task):
         self.set_text(insert='打开个人中心')
         # 打开个人中心
         self.tap((25, 25), 2 * self.bot.config.tapSleep)
-        self.bot.snashot_update_event()
-        player_name = self.bot.gui.player_name()
-        player_name = re.sub('^[^a-zA-Z0-9_\u4e00-\u9fa5]+$', '', player_name)
-        self.set_text(insert='读取玩家名字, {}'.format(player_name))
+        self.set_text(insert='读取玩家名字')
+        for i in range(3):
+            player_name = self.bot.gui.player_name()
+            player_name = re.sub('^[^a-zA-Z0-9_\u4e00-\u9fa5]+$', '', player_name)
+            if len(player_name) > 0:
+                break
+            time.sleep(self.bot.config.tapSleep)
+        self.set_text(insert='读取到玩家名字, {}'.format(player_name))
         # self.set_text(name=player_name)
         self.device.nickname = player_name
+        self.bot.snashot_update_event()
         self.bot.config_update_event(config=self.bot.config, prefix=self.device.save_file_prefix)
         return next_task
