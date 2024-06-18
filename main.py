256a7888297afdf7a112256faae2d53969395085
Kingsley Yau <Kingsleyyau@gmail.com>
2024-06-18 15:50:19 +0800

    Sync

--- main.py
+++ main.py
@@ -26,29 +26,36 @@ def main():
     
 def api_deamon(args):
     bot = get_bot()
-    filepath = 'record.txt'
+    filepath = args.api_deamon_file
     while True:
         try:
-            with open(filepath, 'r', encoding='utf-8') as f:
+            with open(filepath, 'r+', encoding='utf-8') as f:
                 lines = f.readlines()
                 if len(lines) > 0:
-                    line = lines[0]
-                    item = json.loads(line)
-                    log('item', item)
-                    
-                    record = item['record']
-                    args.run_type = 'request_title'
-                    args.title = record['title']
-                    args.server = record['server']
-                    args.x = record['x']
-                    args.y = record['y']
-                    
-                    run_api(args, bot)
-                    # with open(filepath, 'w', encoding='utf-8') as nf:
-                    #     nf.writelines(lines)
+                    try:
+                        line = lines[0]
+                        item = json.loads(line)
+                        log('item', item)
+                        
+                        # 执行记录
+                        record = item['record']
+                        args.run_type = 'request_title'
+                        args.title = record['title']
+                        args.server = record['server']
+                        args.x = record['x']
+                        args.y = record['y']
+                        run_api(args, bot)
+                    except BaseException as e:
+                        log('item', item, e)
+                    finally:
+                        # 移除记录
+                        lines = lines[1:]
+                        with open(filepath, 'w', encoding='utf-8') as nf:
+                            nf.writelines(lines)
+                            
         except BaseException as e:
             log(e)
-        time.sleep(5)
+        time.sleep(1)
         
 def api(args):
     run_api(args)
@@ -67,63 +74,12 @@ class HttpHandler(BaseHTTPRequestHandler):
             'errno':0,
             'errmsg':"",
         }
-        if path == '/api/rok_title':
-            if pr:
-                args = urllib.parse.parse_qs(pr.query)
-                print(pr.path, args)
-                query = {k: v[0] for k, v in args.items()}
-                query_json = json.dumps(query, ensure_ascii=False)
-                
-                record = {
-                    'server':query['server'],
-                    'x':query['x'],
-                    'y':query['y'],
-                    'title':query['title'],
-                }
-                print(pr.path, record)
-                
-                result = False
-                try:
-                    with open('record.txt', 'r') as f:
-                        lines = f.readlines()
-                        for line in lines:
-                            if line.find(query_json) != -1:
-                                item = json.loads(line)
-                                rp['errno'] = 1
-                                rp['errmsg'] = '请勿重复提交, 上次提交时间' + item['summit_time']
-                                result = True
-                                break
-                except BaseException as e:
-                    print(e)
-                 
-                try:   
-                    if not result:
-                        summit_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-                        item = {
-                            'record':record,
-                            'summit_time':summit_time
-                        }
-                        line = json.dumps(item)
-                        print('Add', line)
-                        with open('record.txt', 'a+') as f:
-                            f.write(line + '\n')
-                except BaseException as e:
-                    print(e)   
-        elif path == '/api/rok_title_queue':
-            data = {
-                'size':0,
-                'queue':[],
-            }
-            with open('record.txt', 'r') as f:
-                lines = f.readlines()
-                for line in lines:
-                    item = json.loads(line)
-                    print('Read', item)
-                    data['queue'].append(item)
-                data['size'] = len(lines)
-            rp['data'] = data          
-        else:
-            rp['errno'] = 1
+        if pr:
+            args = urllib.parse.parse_qs(pr.query)
+            print(pr.path, args)
+            query = {k: v[0] for k, v in args.items()}
+            query_json = json.dumps(query, ensure_ascii=False)
+            print(pr.path, query_json)
             
         self.send_response(200)
         self.send_header('Content-type', 'text/json; charset=utf-8')
@@ -145,7 +101,8 @@ if __name__ == '__main__':
     parser.add_argument("--x", type=str, default='0', help="x")
     parser.add_argument("--y", type=str, default='0', help="y")
     parser.add_argument("--player", type=int, default='1', help="player")
-    parser.add_argument("--api_deamon", type=str2bool, default=False, help="api mode")
+    parser.add_argument("--api_deamon", type=str2bool, default=False, help="api deamon mode")
+    parser.add_argument("--api_deamon_file", type=str, default='record.txt', help="api record file")
     args = parser.parse_args()
     if args.api:
         api(args)
--- web/rok_deamon.sh
+++ web/rok_deamon.sh
@@ -4,4 +4,5 @@
 
 ps -ef | grep rok.sh | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
 sleep 10
-nohup /root/Max/project/rok/web/rok.sh > /root/Max/project/rok/web/rok.log 2>&1 &
+#nohup /root/Max/project/rok/web/rok.sh > /root/Max/project/rok/web/rok.log 2>&1 &
+nohup /root/Max/project/rok/web/rok_api.sh > /root/Max/project/rok/web/rok.log 2>&1 &
